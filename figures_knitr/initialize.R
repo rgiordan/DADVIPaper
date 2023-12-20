70b1f626b4d0cf3909646aa59085f6482c7803c0
Ryan Giordano <rgiordan@gmail.com>
2023-12-20 14:16:54 -0800

    remove jmlr subdirectory from knitr

--- figures_knitr/initialize.R
+++ figures_knitr/initialize.R
@@ -11,7 +11,7 @@ library(scales)
 
 # This must be run from within the git repo, obviously.
 git_repo_loc <- system("git rev-parse --show-toplevel", intern=TRUE)
-paper_directory <- file.path(git_repo_loc, "jmlr")
+paper_directory <- file.path(git_repo_loc)
 data_path <- file.path(paper_directory, "experiments_data")
 
 # Plotting and processing functions common to multiple figures
